ea9694be0c780f7674f84e1ca8a549c022b8304e
cesarsiguencia <cesar.siguencia@gmail.com>
2023-04-24 10:05:03 -0400

    added python pages with their routes using blueprint and flask

---
+++ app/__init__.py
@@ -0,0 +1,29 @@
+from app.routes import home, dashboard
+
+
+from flask import Flask
+
+def create_app(test_config=None):
+    # set up createApp
+    app = Flask(__name__, static_url_path='/')
+    app.url_map.strict_slashes = False
+    app.config.from_mapping(
+        SECRET_KEY='super_secret_key'
+    )
+
+    @app.route('/hello')
+    def hello():
+        return 'hello world'
+    
+    # register routes
+    app.register_blueprint(home)
+    app.register_blueprint(dashboard)
+
+    return app
+
+
+# The app should serve any static resources from the root directory and not from the default /static directory.
+
+# Trailing slashes are optional (meaning that /dashboard and /dashboard/ load the same route).
+
+# The app should use the key called 'super_secret_key' when creating server-side sessions.
