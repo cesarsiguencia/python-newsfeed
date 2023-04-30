c884e502fe131d3f4c12ce4668cceaecdaaadf7b
cesarsiguencia <cesar.siguencia@gmail.com>
2023-04-30 18:55:12 -0400

    added scalar_subquery() function for votes

--- app/models/Post.py
+++ app/models/Post.py
@@ -11,7 +11,7 @@ class Post(Base):
   post_url = Column(String(100), nullable=False)
   user_id = Column(Integer, ForeignKey('users.id'))
   vote_count = column_property(
-    select(func.count(Vote.id)).where(Vote.post_id == id)
+    select(func.count(Vote.id)).where(Vote.post_id == id).scalar_subquery()
   )
   created_at = Column(DateTime, default=datetime.now)
   updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
