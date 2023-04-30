d29b89e6079abb636caf2ff90bbbcdd298f7f48b
cesarsiguencia <cesar.siguencia@gmail.com>
2023-04-29 20:26:19 -0400

    added authorization utils function

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -14,12 +14,15 @@ Base = declarative_base()
 
 # loads seeds
 def init_db(app):
-    Base.metadata.create_all(engine)
+    Base.metadata.create_all(engine) #build tables
+    #close connection
     app.teardown_appcontext(close_db)
 
 # gets info from database
 def get_db():
+    #so that we use the same session instead of returning Session everytime we declate this function, which would create a new Session on top of an already existing one
     if 'db' not in g:
+        # store db connection in app context
         g.db = Session()
     return g.db
 
