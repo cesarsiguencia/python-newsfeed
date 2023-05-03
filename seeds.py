ea7f94dda8fb30dff224719882e3e0410511f53b
cesarsiguencia <cesar.siguencia@gmail.com>
2023-05-03 07:11:43 -0400

    uploading new seeds

--- seeds.py
+++ seeds.py
@@ -1,5 +1,5 @@
-from app.models import User, Post, Comment, Vote
-from app.db import Session, Base, engine
+from models import User, Post, Comment, Vote
+from db import Session, Base, engine
 
 # drop and rebuild tables
 Base.metadata.drop_all(engine)
@@ -8,55 +8,36 @@ Base.metadata.create_all(engine)
 db = Session()
 
 # insert users
-# db.add_all([
-#   User(username='CesarSig', email='cesar@gmail.com"', password='abcd1234'),
-#   User(username='Samantha', email='sam@gmail.com"', password='1234abcd'),
-#   User(username='Anna', email='anna@gmail.com', password='password'),
-# ])
-# db.commit()
-
-db.add(
-  User(username='CesarSig', email='cesar@gmail.com"', password='abcd1234')
-)
+db.add_all([
+  User(username='CesarSig', email='cesar@gmail.com"', password='abcd1234'),
+  User(username='Samantha', email='sam@gmail.com"', password='1234abcd'),
+  User(username='Anna', email='anna@gmail.com', password='password'),
+])
 db.commit()
 
-db.add(
-  User(username='Samantha', email='sam@gmail.com"', password='1234abcd')
-)
-db.commit()
-
-db.add(
-  User(username='Anna', email='anna@gmail.com', password='password')
-)
-db.commit()
 
 # insert posts
 db.add_all([
-  Post(title='Donec posuere metus vitae ipsum', post_url='https://buzzfeed.com/in/imperdiet/et/commodo/vulputate.png', user_id=1),
-  Post(title='Morbi non quam nec dui luctus rutrum', post_url='https://nasa.gov/donec.json', user_id=1),
-  Post(title='Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue', post_url='https://europa.eu/parturient/montes/nascetur/ridiculus/mus/etiam/vel.aspx', user_id=2),
-  Post(title='Nunc purus', post_url='http://desdev.cn/enim/blandit/mi.jpg', user_id=3),
-  Post(title='Pellentesque eget nunc', post_url='http://google.ca/nam/nulla/integer.aspx', user_id=2)
+  Post(title="My first post", post_url="https://www.apple.com/", user_id=1),
+  Post(title="Her first post", post_url="https://www.youtube.com/", user_id=2),
+  Post(title='Please checkout my portfolio', post_url="https://cesarsiguencia.github.io/my-react-portfolio/", user_id=1)
 ])
 db.commit()
 
 # insert comments
 db.add_all([
-  Comment(comment_text='Nunc rhoncus dui vel sem.', user_id=1, post_id=2),
-  Comment(comment_text='Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.', user_id=1, post_id=3),
-  Comment(comment_text='Aliquam erat volutpat. In congue.', user_id=2, post_id=1),
-  Comment(comment_text='Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', user_id=2, post_id=3),
-  Comment(comment_text='In hac habitasse platea dictumst.', user_id=3, post_id=3)
+  Comment(comment_text="Congratulations on your first post!", user_id=2, post_id=1),
+  Comment(comment_text="Jealous of your portfolio! :)", user_id=2, post_id=3),
+  Comment(comment_text="Teach me how to do this!", user_id=3, post_id=3),
 ])
 db.commit()
 
 # insert votes
 db.add_all([
   Vote(user_id=1, post_id=2),
-  Vote(user_id=1, post_id=4),
-  Vote(user_id=2, post_id=4),
-  Vote(user_id=3, post_id=4),
-  Vote(user_id=3, post_id=2)
+  Vote(user_id=2, post_id=1),
+  Vote(user_id=2, post_id=3),
+  Vote(user_id=3, post_id=3)
 ])
 db.commit()
 
