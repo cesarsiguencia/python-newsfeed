d0f591b5b8e78a400650c4e183e7ef0a75d329c4
cesarsiguencia <cesar.siguencia@gmail.com>
2023-08-10 10:25:06 -0400

    added correct seeds

--- seeds.py
+++ seeds.py
@@ -53,10 +53,12 @@ db.commit()
 # insert votes
 db.add_all([
   Vote(user_id=1, post_id=2),
-  Vote(user_id=1, post_id=4),
+  Vote(user_id=2, post_id=1),
+  Vote(user_id=2, post_id=3),
+  Vote(user_id=3, post_id=3),
+  Vote(user_id=1, post_id=5),
+  Vote(user_id=2, post_id=5),
   Vote(user_id=2, post_id=4),
-  Vote(user_id=3, post_id=4),
-  Vote(user_id=3, post_id=2)
 ])
 db.commit()
 
